0816aa612659bc5db5cfce4a16ba3358ca891cd2
Keenan Payne <kpayne@slackrmedia.com>
2014-11-05 19:23:48 -0800

    Remove AdBlock code

--- js/site.js
+++ js/site.js
@@ -2,11 +2,3 @@
 if (!Modernizr.svg) {
   $(".logo img").attr("src", "/images/logo.png");
 }
-
-function determineAdBlock() {
-    if ($('#fusionads').filter(':visible').length == 0 || $('#fusionads').filter(':hidden').length > 0) {
-        alert("Please stop using adblock");
-    }
-}
-
-determineAdBlock();
